6339189ee5e0520281e51aaa0f9fbd5da468c284
Zitajake1993 <zitajacob221@gmail.com>
2023-09-09 22:53:33 +0100

    commit

---
+++ password generator/sort fruit/index.js
@@ -0,0 +1,11 @@
+let fruit = ["🍎", "🍊", "🍎", "🍎", "🍊"]
+let appleShelf = document.getElementById("apple-shelf")
+let orangeShelf = document.getElementById("orange-shelf")
+for (let i=0; i < fruit.length; i++) {
+if (i="🍎") {
+    appleShelf.textContent = fruit[appleShelf]
+}
+else {
+    orangeShelf.textContent = fruit[orangeShelf]
+}
+}
